5f7ed30e4914e0e74f642afddc741556b55d29da
myrlyn13 <myranda.lynne13@gmail.com>
2024-07-23 23:18:28 -0400

    CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,26 +4,28 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    totalQuantity: 0
   },
   reducers: {
     addItem: (state, action) => {
-      const { name, image, cost } = action.payload;
-      const existingItem = state.items.find((item) => item.name === name);
+      const {name, image, cost} = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
       if (existingItem) {
         existingItem.quantity++;
       } else {
         state.items.push({ name, image, cost, quantity: 1 });
       }
+      state.totalQuantity++;
     },
     removeItem: (state, action) => {
-      state.items = state.items.filter((item) => item.name !== action.payload.name);
+      state.items = state.items.filter(item => item.name !== action.payload.name);
+      state.totalQuantity--;
     },
     updateQuantity: (state, action) => {
-      console.log(action.payload);
       const { name, quantity } = action.payload;
-      const existingItem = state.items.find((item) => item.name === name);
-      if (existingItem) {
-        existingItem.quantity = quantity;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
       }
     },
   },
